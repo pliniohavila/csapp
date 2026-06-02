1317728ad7775d23d0db455f1b3282a66c5b4711
pliniohavila <pliniohrcode@gmail.com>
2026-06-02 13:01:15 -0300

    272

--- 2_ch/homeworks/272.c
+++ 2_ch/homeworks/272.c
@@ -1,46 +1,50 @@
 #include <assert.h>
 #include <string.h>
+#include <stdio.h>
 
 void copy_int(int val, void *buf, int maxbytes)
 {
-  if ((maxbytes - sizeof(val)) >= 0) {
+  // sizeof return a value of type unsigned integer type
+
+  // if ((maxbytes - (int)sizeof(val)) >= 0) {
+  if (maxbytes >= sizeof(val)) {
     memcpy(buf, (void *) &val, sizeof(val));
   }
 }
 
 int main(void)
 {
-    int out;
+  int out;
 
-    /* ---------- Espaço suficiente ---------- */
+  /* ---------- Espaço suficiente ---------- */
 
-    out = 0;
-    copy_int(12345, &out, sizeof(int));
-    assert(out == 12345);
+  out = 0;
+  copy_int(12345, &out, sizeof(int));
+  assert(out == 12345);
 
-    /* ---------- Espaço maior ---------- */
+  /* ---------- Espaço maior ---------- */
 
-    out = 0;
-    copy_int(67890, &out, 100);
-    assert(out == 67890);
+  out = 0;
+  copy_int(67890, &out, 100);
+  assert(out == 67890);
 
-    /* ---------- Sem espaço ---------- */
+  /* ---------- Sem espaço ---------- */
 
-    out = -1;
-    copy_int(555, &out, sizeof(int) - 1);
-    assert(out == -1);
+  out = -1;
+  copy_int(555, &out, sizeof(int) - 1);
+  assert(out == -1);
 
-    /* ---------- Espaço zero ---------- */
+  /* ---------- Espaço zero ---------- */
 
-    out = -1;
-    copy_int(777, &out, 0);
-    assert(out == -1);
+  out = -1;
+  copy_int(777, &out, 0);
+  assert(out == -1);
 
-    /* ---------- Valor negativo ---------- */
+  /* ---------- Valor negativo ---------- */
 
-    out = 0;
-    copy_int(-42, &out, sizeof(int));
-    assert(out == -42);
+  out = 0;
+  copy_int(-42, &out, sizeof(int));
+  assert(out == -42);
 
-    return 0;
+  return 0;
 }
